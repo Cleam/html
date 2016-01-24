3fffe58d83c62d4c2deccc266a248b16f914f934
lizhigao <lizhigao@021.com>
2016-01-24 18:27:43 +0800

    add eastday-img-rank and update ceshi

--- ceshi/mycs/server.php
+++ ceshi/mycs/server.php
@@ -1,18 +1,4 @@
-<?php
-$f = new form();
-$flag = $f->validate($_GET['name']);
-echo var_dump($flag);
 
-class form
-{
-    function validate($name)
-    {
-        if (!isset($name) && !empty($name)) {
-            return true;
-        }
-        return false;
-    }
-}
 
 
 
