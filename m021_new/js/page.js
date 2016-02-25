ab6e818d8ebddf8dba8121f4f9813a825b38f2dd
lizhigao <lizhigao@021.com>
2016-02-25 18:42:04 +0800

    update eastday_tianqi_m and m021_new

--- m021_new/js/page.js
+++ m021_new/js/page.js
@@ -1,105 +1,127 @@
 Zepto(function() {
+    /**
+     * 输入框清空功能实现
+     */
+    (function(){
+        var $searchInput = $('#J_search_input');
+        $searchInput.on('keyup', function(){
+            if($.trim($searchInput.val()) !== ''){
+                
+            } else {
+                
+            }
+        });
+
+    })();
+
+
     /* 热站导航宽窄屏判断功能实现 */
-    var $hotSiteMore = $('#J_hot_site_more');
-    $hotSiteMore.click(function(e) { e.preventDefault(); });
-    $hotSiteMore.on('touchstart mousedown', function(e) {
-        e.preventDefault();
-        var $this = $(this);
-        if($this.data('state') == '0'){
-            $this.parent().addClass('expand');
-            $this.data('state', '1');
-            $this.find('span').text('收起');
-        } else {
-            $this.parent().removeClass('expand');
-            $this.data('state', '0');
-            $this.find('span').text('更多');
-        }
-    });
-    /*$(window).resize(function(){
-        var screenWidth = window.screen.width,
-            $hotSiteItems =  $('.J-hot-site-list').children();
-        if(screenWidth <= 375){
-            $hotSiteItems.addClass('sm');
-            $hotSiteItems.eq(9).hide();
-            $hotSiteItems.eq(10).hide();
-        } else {
-            $hotSiteItems.removeClass('sm');
-            $hotSiteItems.eq(9).show();
-            $hotSiteItems.eq(10).show();
-        }
-    });
-    $(window).resize();*/
+    (function(){
+        var $hotSiteMore = $('#J_hot_site_more');
+        $hotSiteMore.click(function(e) { e.preventDefault(); });
+        $hotSiteMore.on('touchstart mousedown', function(e) {
+            e.preventDefault();
+            var $this = $(this);
+            if($this.data('state') == '0'){
+                $this.parent().addClass('expand');
+                $this.data('state', '1');
+                $this.find('span').text('收起');
+            } else {
+                $this.parent().removeClass('expand');
+                $this.data('state', '0');
+                $this.find('span').text('更多');
+            }
+        });
+        /*$(window).resize(function(){
+            var screenWidth = window.screen.width,
+                $hotSiteItems =  $('.J-hot-site-list').children();
+            if(screenWidth <= 375){
+                $hotSiteItems.addClass('sm');
+                $hotSiteItems.eq(9).hide();
+                $hotSiteItems.eq(10).hide();
+            } else {
+                $hotSiteItems.removeClass('sm');
+                $hotSiteItems.eq(9).show();
+                $hotSiteItems.eq(10).show();
+            }
+        });
+        $(window).resize();*/
+    })();
 
     /* 酷站导航tab切换功能实现 */
-    var tabsSwiper = new Swiper('#J_tabs_container', {
-        speed: 300,
-        autoHeight: true,
-        onSlideChangeStart: function() {
-            var $activeSlide = $('#J_tabs_container').find('.swiper-slide').eq(tabsSwiper.activeIndex),
-                $slideWrapper = $activeSlide.parent();
-            $slideWrapper.height($activeSlide.height());
+    (function(){
+        var tabsSwiper = new Swiper('#J_tabs_container', {
+            speed: 300,
+            autoHeight: true,
+            onSlideChangeStart: function() {
+                var $activeSlide = $('#J_tabs_container').find('.swiper-slide').eq(tabsSwiper.activeIndex),
+                    $slideWrapper = $activeSlide.parent();
+                $slideWrapper.height($activeSlide.height());
+                $(".cs-tabs").children('.active').removeClass('active');
+                $(".cs-tabs").children().eq(tabsSwiper.activeIndex).addClass('active');
+            }
+        });
+        $(".cs-tabs a").click(function(e) { e.preventDefault(); });
+        $(".cs-tabs a").on('touchstart mousedown', function(e) {
+            e.preventDefault();
+            var $this = $(this);
             $(".cs-tabs").children('.active').removeClass('active');
-            $(".cs-tabs").children().eq(tabsSwiper.activeIndex).addClass('active');
-        }
-    });
-    $(".cs-tabs a").click(function(e) { e.preventDefault(); });
-    $(".cs-tabs a").on('touchstart mousedown', function(e) {
-        e.preventDefault();
-        var $this = $(this);
-        $(".cs-tabs").children('.active').removeClass('active');
-        $this.addClass('active');
-        tabsSwiper.slideTo($this.index());
-    });
-
-    /* 酷站【更多】功能实现 */
-    $('.J-more').click(function(e) { e.preventDefault(); });
-    $('.J-more').on('touchstart mousedown', function(e) {
-        e.preventDefault();
-        var $this = $(this);
-        $this.parent().parent().parent().css('height', 'auto');
-        if ($this.hasClass('active')) {
-            $this.parent().next().hide();
-            $this.removeClass('active');
-        } else {
-            $this.parent().next().show();
             $this.addClass('active');
-        }
-    });
+            tabsSwiper.slideTo($this.index());
+        });
+
+        /* 酷站【更多】功能实现 */
+        $('.J-more').click(function(e) { e.preventDefault(); });
+        $('.J-more').on('touchstart mousedown', function(e) {
+            e.preventDefault();
+            var $this = $(this);
+            $this.parent().parent().parent().css('height', 'auto');
+            if ($this.hasClass('active')) {
+                $this.parent().next().hide();
+                $this.removeClass('active');
+            } else {
+                $this.parent().next().show();
+                $this.addClass('active');
+            }
+        });
+    })();
 
     /* 头条新闻菜单显示隐藏功能实现 */
-    var $ttNews = $('#J_tt_news'),
-        $ttNewsNav = $ttNews.children('.tt-news-nav'),
-        tnnHeight = $ttNewsNav.height(),
-        flag = true;
-    $ttNewsNav.children('.home').on('click', function(e) { e.preventDefault(); });
-    $ttNewsNav.children('.home').on('touchstart mousedown', function(e) {
-        e.preventDefault();
-        goTop(200);
-    });
-    $(window).on('scroll', function() {
-        var offsetTop = $ttNews.offset().top;
-        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        if (scrollTop + tnnHeight >= offsetTop) {
-            if (!flag) {
-                return; }
-            flag = false;
-            $ttNewsNav.css('zIndex', 9);
-            $ttNewsNav.animate({
-                'opacity': 1,
-                'top': '0px'
-            }, 400, 'linear');
-        } else {
-            if (flag) {
-                return; }
+    (function(){
+        var $ttNews = $('#J_tt_news'),
+            $ttNewsNav = $ttNews.children('.tt-news-nav'),
+            tnnHeight = $ttNewsNav.height(),
             flag = true;
-            $ttNewsNav.animate({
-                'opacity': 0,
-                'top': '-' + tnnHeight + 'px'
-            }, 200, 'linear', function() {
-                $ttNewsNav.css('zIndex', -1);
-            });
-        }
-    });
+        $ttNewsNav.children('.home').on('click', function(e) { e.preventDefault(); });
+        $ttNewsNav.children('.home').on('touchstart mousedown', function(e) {
+            e.preventDefault();
+            goTop(200);
+        });
+        $(window).on('scroll', function() {
+            var offsetTop = $ttNews.offset().top;
+            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            if (scrollTop + tnnHeight >= offsetTop) {
+                if (!flag) {
+                    return; }
+                flag = false;
+                $ttNewsNav.css('zIndex', 9);
+                $ttNewsNav.animate({
+                    'opacity': 1,
+                    'top': '0px'
+                }, 400, 'linear');
+            } else {
+                if (flag) {
+                    return; }
+                flag = true;
+                $ttNewsNav.animate({
+                    'opacity': 0,
+                    'top': '-' + tnnHeight + 'px'
+                }, 200, 'linear', function() {
+                    $ttNewsNav.css('zIndex', -1);
+                });
+            }
+        });
+    })();
 
     //返回顶部动画
     //goTop(500);//500ms内滚回顶部
@@ -129,14 +151,5 @@ Zepto(function() {
         ani();
     }
 
-    /* 视频播放 */
-    /*var $videoCover = $('.video-cover');
-    $videoCover.on('click', function(e) { e.preventDefault(); });
-    $videoCover.on('touchstart mousedown', function(e) {
-        e.preventDefault();
-        var $this = $(this);
-        $this.hide();
-        $this.parent().append('<video x-webkit-airplay="true" webkit-playsinline="true" preload="auto" autoplay="autoplay" controls="controls"><source src="video.mp4" type="video/mp4" /></video>');
-    });*/
 
 });
