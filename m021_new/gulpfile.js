ba2cf5b97004cf949771aef838becb29986a0740
lizhigao <lizhigao@021.com>
2016-03-08 22:04:58 +0800

    update m021_new and add waterFall

--- m021_new/gulpfile.js
+++ m021_new/gulpfile.js
@@ -9,7 +9,10 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     clean = require('gulp-clean'),             //清空文件夹
-    livereload = require('gulp-livereload'),
+    tinylr = require('tiny-lr'),               //livereload
+    server = tinylr(),
+    port = 35729,
+    livereload = require('gulp-livereload'),   //livereload
     del = require('del');
 // HTML处理
 /*
@@ -21,7 +24,7 @@ gulp.task('html', function() {
     var htmlSrc = './src/*.html',
         htmlDst = './dist/';
     gulp.src(htmlSrc)
-        // .pipe(livereload(server))
+        .pipe(livereload(server))
         .pipe(gulp.dest(htmlDst));
 });
 
@@ -35,6 +38,7 @@ gulp.task('css', function () {
 		.pipe(gulp.dest(cssDst))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
+    .pipe(livereload(server))
     .pipe(gulp.dest(cssDst))
 		.pipe(notify({ message: 'Styles task complete' }));
 });
@@ -50,6 +54,7 @@ gulp.task('js', function() {
     .pipe(gulp.dest(jsDst))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
+    .pipe(livereload(server))
     .pipe(gulp.dest(jsDst))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
@@ -60,6 +65,7 @@ gulp.task('img', function() {
       imgDst = './dist/img';
   return gulp.src(imgSrc)
     .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
+    .pipe(livereload(server))
     .pipe(gulp.dest(imgDst));
     // .pipe(notify({ message: 'Images task complete' }));
 });
@@ -78,18 +84,36 @@ gulp.task('default', ['clean'], function() {
 }); 
 */
 
- gulp.task('default', ['html', 'css', 'img', 'js']);
+gulp.task('default', ['html', 'css', 'img', 'js']);
 
 // 监听文件
-gulp.task('watch', function() {
-  // Watch .scss files
-  //gulp.watch('src/styles/**/*.scss', ['styles']);
-  // Watch .js files
-  //gulp.watch('src/scripts/**/*.js', ['scripts']);
-  // Watch image files
-  //gulp.watch('src/images/**/*', ['images']);
-  // Create LiveReload server
-  //livereload.listen();
-  // Watch any files in dist/, reload on change
-  //gulp.watch(['dist/**']).on('change', livereload.changed);
+// 监听任务 运行语句 gulp watch
+gulp.task('watch',function(){
+
+    server.listen(port, function(err){
+        if (err) {
+            return console.log(err);
+        }
+
+        // 监听html
+        gulp.watch('./src/*.html', function(event){
+            gulp.run('html');
+        })
+
+        // 监听css
+        gulp.watch('./src/css/*.css', function(){
+            gulp.run('css');
+        });
+
+        // 监听images
+        gulp.watch('./src/img/**/*', function(){
+            gulp.run('img');
+        });
+
+        // 监听js
+        gulp.watch('./src/js/*.js', function(){
+            gulp.run('js');
+        });
+
+    });
 });
