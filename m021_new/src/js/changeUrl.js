66745e208a47ed89841477c503b63202374111f4
Cleam Lee <lizhigao1231@163.com>
2016-04-22 21:31:50 +0800

    021广告更新

--- m021_new/src/js/changeUrl.js
+++ m021_new/src/js/changeUrl.js
@@ -5,19 +5,14 @@
  */
 +function(){
 	if(tt_news_mid === 'pgzs' || tt_news_mid.indexOf('waitui') > -1){
-		/*
-		百度搜索框代码  http://m.baidu.com/?from=1012150a
-		爱淘宝：http://ai.m.taobao.com?pid=mm_113832645_13024572_50938655
-		天猫精选：http://s.click.taobao.com/t?e=m%3D2%26s%3Djokmi2nvR%2FIcQipKwQzePCperVdZeJvipRe%2F8jaAHci5VBFTL4hn2YR%2BfqbSjekxbJxUEh8sgi%2B6Wguv30TCbHxveng1wJL%2BjoLQbbx50iygSHBHkHELt%2BVKDnkZVfnBvoet87lGTGGIGrHgCgZNHPhXpSX4ya3U
-		聚划算：http://s.click.taobao.com/t?e=m%3D2%26s%3DIzVpx29jtG9w4vFB6t2Z2iperVdZeJviEViQ0P1Vf2lyINtkUhsv0M9EwUL6BAhEUukTSLZXHV%2B9AmARIwX9K3HFd9TCmEKTeEV%2BM%2BWE6mfZL%2FrExw0TRqb6h%2BauVgdyXhvQRrBlV3jGDmntuH4VtA%3D%3D&pid=mm_113832645_13024572_50940954
-		京东：http://union.click.jd.com/jdc?e=&p=AyIHVCtaJQMiQwpDBUoyS0IQWlALHE4YDk5ER1xOGWV5PRxpdH4TYj59dGVgHnA%2FY2FkYzRdVxkyFwZRE1kWChAFZRtaFAMTBFUTXRMyImEoKxB7AyIHZRteEQsUD10fWBAEEwJlHGtIVlNXDV0LSlRPQwsraw%3D%3D&t=W1dCFBBFC14NXAAECUteDA%3D%3D
-		 */
-		var links = ['aitaobao', 'tianmao', 'juhuasuan', 'jingdong'],
+		var links = ['aitaobao', 'tianmao', 'juhuasuan', 'jingdong', 'baidu', 'youxizhongxin'],
 		 	urls = [
 		 		'http://ai.m.taobao.com?pid=mm_113832645_13024572_50938655',
 		 		'http://s.click.taobao.com/t?e=m%3D2%26s%3Djokmi2nvR%2FIcQipKwQzePCperVdZeJvipRe%2F8jaAHci5VBFTL4hn2YR%2BfqbSjekxbJxUEh8sgi%2B6Wguv30TCbHxveng1wJL%2BjoLQbbx50iygSHBHkHELt%2BVKDnkZVfnBvoet87lGTGGIGrHgCgZNHPhXpSX4ya3U',
 		 		'http://s.click.taobao.com/t?e=m%3D2%26s%3DIzVpx29jtG9w4vFB6t2Z2iperVdZeJviEViQ0P1Vf2lyINtkUhsv0M9EwUL6BAhEUukTSLZXHV%2B9AmARIwX9K3HFd9TCmEKTeEV%2BM%2BWE6mfZL%2FrExw0TRqb6h%2BauVgdyXhvQRrBlV3jGDmntuH4VtA%3D%3D&pid=mm_113832645_13024572_50940954',
-		 		'http://union.click.jd.com/jdc?e=&p=AyIHVCtaJQMiQwpDBUoyS0IQWlALHE4YDk5ER1xOGWV5PRxpdH4TYj59dGVgHnA%2FY2FkYzRdVxkyFwZRE1kWChAFZRtaFAMTBFUTXRMyImEoKxB7AyIHZRteEQsUD10fWBAEEwJlHGtIVlNXDV0LSlRPQwsraw%3D%3D&t=W1dCFBBFC14NXAAECUteDA%3D%3D'
+		 		'http://union.click.jd.com/jdc?e=&p=AyIHVCtaJQMiQwpDBUoyS0IQWlALHE4YDk5ER1xOGWV5PRxpdH4TYj59dGVgHnA%2FY2FkYzRdVxkyFwZRE1kWChAFZRtaFAMTBFUTXRMyImEoKxB7AyIHZRteEQsUD10fWBAEEwJlHGtIVlNXDV0LSlRPQwsraw%3D%3D&t=W1dCFBBFC14NXAAECUteDA%3D%3D',
+		 		'https://m.baidu.com/?from=1012150a',
+		 		'http://tongji.51pgzs.com/51pgzs/appid/index.html'
 		 	];
 		$('input[name=from]').val('1012150a');
 		for (var i = 0; i < links.length; i++) {
