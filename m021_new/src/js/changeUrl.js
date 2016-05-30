c4cbd8f13dc5d8b79635ee3914889ebb3a4881b5
Cleam Lee <lizhigao1231@163.com>
2016-05-30 15:14:43 +0800

    m021: bug fixed

--- m021_new/src/js/changeUrl.js
+++ m021_new/src/js/changeUrl.js
@@ -11,8 +11,8 @@
 	 		'http://union.click.jd.com/jdc?e=&p=AyIHVCtaJQMiQwpDBUoyS0IQWlALHE4YDk5ER1xONythJ1QcSgNTeCxNQRtnUmgEVmlOAzUXVyUHEgRTE1kUABM3VRpaFAMRB10bXyUydHplUDUUMhI3VR5cFwobBlYdUhUAEjdSK46cucWD087TtNWatWUraw%3D%3D&t=W1dCFBBFC14NXAAECUte',
 	 		'https://m.baidu.com/?from=1017246b',
 	 		'http://tongji.51pgzs.com/51pgzs/appid/index.html',
-	 		'http://ai.m.taobao.com/index.html?pid=mm_112599953_11408251_55960340',	// 天猫
-	 		'http://s.click.taobao.com/t?e=m%3D2%26s%3DQjzzy9CS31kcQipKwQzePCperVdZeJviK7Vc7tFgwiFRAdhuF14FMYFeZnIfpf7r1aH1Hk3GeOgVKIrKoTqn7QafxTjEMcEf9vrWv5Uq6elXdmLdbxdQT02srC8Mk09eQgZss1jm63jcHtRpEUy6RPalRWTdFmFpJPwiig1bxLMnyi1UQ%2F17I10hO9fBPG8oXH%2BQH9e66Y4%3D'	// 爱淘宝
+	 		'http://s.click.taobao.com/t?e=m%3D2%26s%3DQjzzy9CS31kcQipKwQzePCperVdZeJviK7Vc7tFgwiFRAdhuF14FMYFeZnIfpf7r1aH1Hk3GeOgVKIrKoTqn7QafxTjEMcEf9vrWv5Uq6elXdmLdbxdQT02srC8Mk09eQgZss1jm63jcHtRpEUy6RPalRWTdFmFpJPwiig1bxLMnyi1UQ%2F17I10hO9fBPG8oXH%2BQH9e66Y4%3D',	// 天猫
+	 		'http://ai.m.taobao.com/index.html?pid=mm_112599953_11408251_55960340'	// 爱淘宝
 	 	];
 		$('input[name=from]').val('1017246b');
 		for (var i = 0; i < links.length; i++) {
