7abba8df947093ab3df6c97e7d8eb1905592685d
Cleam Lee <lizhigao1231@163.com>
2016-06-08 20:03:38 +0800

    m021: update qid gg

--- m021_new/src/js/changeUrl.js
+++ m021_new/src/js/changeUrl.js
@@ -6,11 +6,10 @@
 +function(){
 	var links, urls;
 	if(tt_news_mid === 'pgzs' || tt_news_mid.indexOf('waitui') > -1){	// 天明渠道(pgzs和waitui开头的渠道)
-		links = ['jingdong', 'baidu', 'youxizhongxin', 'tianmao', 'aitaobao'];
+		links = ['jingdong', 'baidu', 'tianmao', 'aitaobao'];
 		urls = [
-	 		'http://union.click.jd.com/jdc?e=&p=AyIHVCtaJQMiQwpDBUoyS0IQWlALHE4YDk5ER1xONythJ1QcSgNTeCxNQRtnUmgEVmlOAzUXVyUHEgRTE1kUABM3VRpaFAMRB10bXyUydHplUDUUMhI3VR5cFwobBlYdUhUAEjdSK46cucWD087TtNWatWUraw%3D%3D&t=W1dCFBBFC14NXAAECUte',
-	 		'https://m.baidu.com/?from=1017246b',
-	 		'http://tongji.51pgzs.com/51pgzs/appid/index.html',
+	 		'http://union.click.jd.com/jdc?e=&p=AyIHVCtaJQMiQwpDBUoyS0IQWlALHE4YDk5ER1xONythJ1QcSgNTeCxNQRtnUmgEVmlOAzUXVyUHEgRTE1kUABM3VRpaFAMRB10bXyUydHplUDUUMhI3VR5cFwobBlYdUhUAEjdSK46cucWD087TtNWatWUraw%3D%3D&t=W1dCFBBFC14NXAAECUte',	// 京东
+	 		'https://m.baidu.com/?from=1017246b',	// 百度
 	 		'http://s.click.taobao.com/t?e=m%3D2%26s%3DQjzzy9CS31kcQipKwQzePCperVdZeJviK7Vc7tFgwiFRAdhuF14FMYFeZnIfpf7r1aH1Hk3GeOgVKIrKoTqn7QafxTjEMcEf9vrWv5Uq6elXdmLdbxdQT02srC8Mk09eQgZss1jm63jcHtRpEUy6RPalRWTdFmFpJPwiig1bxLMnyi1UQ%2F17I10hO9fBPG8oXH%2BQH9e66Y4%3D',	// 天猫
 	 		'http://ai.m.taobao.com/index.html?pid=mm_112599953_11408251_55960340'	// 爱淘宝
 	 	];
@@ -18,6 +17,9 @@
 		for (var i = 0; i < links.length; i++) {
 			$('.J-qid-' + links[i]).attr('href', urls[i]);
 		}
+		// 苹果助手的去掉  同城约替换成游戏中心
+		$('.J-tongchengyue').hide();
+		$('.J-youxi').show();
 	} else if(tt_news_mid === 'chenz' || tt_news_mid === 'wy027'){	// chenz渠道
 		links = ['baidu'];
 	 	urls = [
